b29af897795eeecb17e2fa78c0518a288a9d122b
denis <denisalitb@rambler.ru>
2024-03-08 22:36:49 +0100

    interface small improvement

--- src/index.php
+++ src/index.php
@@ -4,6 +4,14 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Denis\TestRepo\StrategyFactory;
 
+const MESSAGE = 'Please provide 5 params: type (cover/contain) w1 h1 w2 h2';
+const VALIDATION = 'Please provide integers for the dimensions';
+
+if (!isset($argv[1]) || $argv[1] === 'help') {
+    echo MESSAGE;
+    exit(0);
+}
+
 try {
     $params = parseParams($argv);
 } catch (Exception $exception) {
@@ -30,9 +38,19 @@ echo "$result[0] * $result[1]";
 exit(0);
 
 function parseParams(array $params): array {
-    if (count($params) !== 6 || $params[1] === 'help') {
-        throw new Exception('Please provide 5 params: type (cover/contain) w1 h1 w2 h2');
+    if (count($params) !== 6) {
+        throw new Exception(MESSAGE);
     }
+
+    if (!is_int($params[1])
+        || !is_int($params[2])
+        || !is_int($params[3])
+        || !is_int($params[4])
+        || !is_int($params[5])
+    ) {
+        throw new Exception(VALIDATION);
+    }
+
     return [
         'type' => $params[1],
         'width1' => $params[2],
